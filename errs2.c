8a936af7a30845ff06668ae30acbd947901c5fbf
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-26 19:53:00 +0300

    Create errs2.c

---
+++ errs2.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+*err_acc_name - prints error by name
+*@name: name of function
+*@line_no: current line number
+*Return: void
+*/
+void err_acc_name(char *name, unsigned int line_no)
+{
+	if (strcmp(name, "pint"))
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_no);
+	if (strcmp(name, "pchar"))
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_no);
+	if (strcmp(name, "zero"))
+		fprintf(stderr, "L%u: division by zero\n", line_no);
+	if (strcmp(name, "out_of_range"))
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_no);
+	else
+		fprintf(stderr, "L%u: can't %s, stack too short\n", name, line_no);
+}
